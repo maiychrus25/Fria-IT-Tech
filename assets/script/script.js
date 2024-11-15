e4c7c03038f9aa437ea97233fe2227604f7f04ec
Nịnh Thanh Khương <165678100+maiychrus25@users.noreply.github.com>
2024-11-15 19:21:54 +0700

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -45,3 +45,52 @@ const goTop = document.querySelector('.go-top');
 goTop.addEventListener('click', () => {
   window.scrollTo(0, 0);
 });
+
+document.querySelectorAll('.fria-responsive-nav__link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    const dropdownMenu = this.nextElementSibling;
+    if (dropdownMenu && dropdownMenu.classList.contains('fria-responsive-nav__dropdown-menu')) {
+      // Toggle the display property of the dropdown menu
+      if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
+        dropdownMenu.style.display = 'block';
+      } else {
+        dropdownMenu.style.display = 'none';
+      }
+    }
+  });
+});
+
+document.querySelectorAll('.fria-responsive-nav__expand').forEach(expandBtn => {
+  expandBtn.addEventListener('click', function (e) {
+    e.preventDefault(); // Ngừng sự kiện mặc định của link
+    const dropdownMenu = this.previousElementSibling; // Chọn phần tử menu dropdown trước đó
+    if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
+      dropdownMenu.style.display = 'block';
+    } else {
+      dropdownMenu.style.display = 'none';
+    }
+  });
+});
+
+const head = document.querySelectorAll('.fria-responsive-nav__menu__dropdown-menu__item__head');
+console.log(head);
+
+head.forEach(item => {
+  let found = false;
+  item.addEventListener('click', () => {
+    head.forEach(item => item.classList.remove('active'));
+
+    found = true;
+    item.classList.add('active');
+
+    const icon = item.querySelector('.fria-responsive-nav__menu__dropdown-menu__item__head__link__icon');
+
+    if (icon.textContent.trim() === '+') {
+      icon.innerHTML = '-';
+    } else if (icon.textContent.trim() === '-') {
+      icon.innerHTML = '+';
+    }
+
+  });
+});
+
